35fc14c6935e0fb4248bd00a0edaa9cd6819b74e
Luke Dary <ldary@redhat.com>
2021-11-06 11:35:21 -0400

    Update WS

--- wsserver.ts
+++ wsserver.ts
@@ -1,10 +1,16 @@
 class User {
-  constructor() {
+  constructor(socket:WebSocket) {
     this.id = crypto.getRandomValues(new Uint8Array(8));
-    addEventListener('update', e => console.log(this.id, 'heard', JSON.stringify(e)));
+    addEventListener('update', ((e:CustomEvent) => {
+        if (<CustomEvent>e.detail && socket.readyState === socket.OPEN) {
+            socket.send(JSON.stringify(e.detail));
+        }
+    }) as EventListener);
   }
-  id;
+  id:Uint8Array;
 }
+
+const users = new Map<Uint8Array, User>()
 
 const listener = await Deno.listen({ port: 3000 });
 for await(const conn of listener) {
@@ -16,19 +22,63 @@ async function dispatchConnection(conn: Deno.Conn) {
     if (request.headers.get('upgrade') === 'websocket') {
       const { socket, response } = Deno.upgradeWebSocket(request);
       socket.onopen = () => {
-
-        socket.send("Hello World!");
+        const newUser = new User(socket);
+        users.set(newUser.id, newUser);
+        socket.send(`Hello New User! Your ID: ${newUser.id}`);
       };
       socket.onmessage = (e) => {
-        console.log(e.data);
-        socket.send(`Got: ${e.data}`);
+        try {
+            dispatchEvent(new CustomEvent('update', {
+                detail: JSON.parse(e.data)
+            }));
+        } catch {
+            dispatchEvent(new CustomEvent('update', {
+                detail: e.data
+            }));
+        }
       };
-      socket.onclose = () => console.log("WebSocket has been closed.");
-      socket.onerror = (e) => console.error("WebSocket error:", e);
       respondWith(response);
     } else {
 
-      respondWith(new Response('<h1>WOO!</h1>', {
+        respondWith(new Response(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Chapellerie</title>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>  
+        <body>
+        <h1>WOO!</h1>
+        <label>Message: <input id="message"></label>
+        <button id="send">Send</button>
+        <ul id="list">
+        </ul>
+        <script>
+        function createWebSocket(url) { 
+            const ws = new WebSocket(url); 
+            ws.onopen = e => console.log('OPENED',e.data); 
+            ws.onmessage = (e) => { 
+                try { 
+                    const li = document.createElement('li');
+                    li.innerHTML = e.data;
+                    document.getElementById('list').appendChild(li);
+                } catch { 
+                    console.log('TEXT:',e.data);
+                }
+            }; 
+            ws.onclose = e => console.log('Socket closed'); 
+            return ws;
+        }
+        const conn = createWebSocket('ws://localhost:3000');
+        document.getElementById('send').addEventListener('click', e => {
+            const msg = document.getElementById('message').value;
+            conn.send(msg);
+        });
+        </script>
+        </body>
+        </html>`, {
         headers: { 'content-type': 'text/html' }
       }));
     }
@@ -36,14 +86,18 @@ async function dispatchConnection(conn: Deno.Conn) {
   }
 }
 
-// const httpConn = Deno.serveHttp(await conn.accept());
-
-// const e = await httpConn.nextRequest();
-
-// if (e) {
-//   console.log(JSON.stringify(e.request));
-//   const { socket, response } = Deno.upgradeWebSocket(e.request);
-//   
-//   e.respondWith(response);
-// }
-
+/*
+function createWebSocket(url) { 
+    const ws = new WebSocket(url); 
+    ws.onopen = e => console.log('OPENED',e.data); 
+    ws.onmessage = (e) => { 
+        try { 
+            console.log('JSON MESSAGE:',JSON.parse(e.data));
+        } catch { 
+            console.log('TEXT:',e.data);
+        }
+    }; 
+    ws.onclose = e => console.log('Socket closed'); 
+    return ws;
+}
+*/
