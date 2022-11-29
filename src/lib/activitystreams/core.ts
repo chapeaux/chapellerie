77a411f4076631285582723abcca5b220700f70f
Luke Dary <ldary@redhat.com>
2022-11-29 15:06:55 -0500

    Initial query and storage

---
+++ src/lib/activitystreams/core.ts
@@ -0,0 +1,35 @@
+export class ActivityStreamObject {
+    // attachment: Array<ActivityStreamObject|ActivityStreamLink>;
+    // attributedTo: Array<ActivityStreamObject|ActivityStreamLink>;
+    // audience: Array<ActivityStreamObject|ActivityStreamLink>;
+    // content: string | Record<string,string>;
+    // context: ActivityStreamObject|ActivityStreamLink;
+    // name: string | Record<string,string>
+    // endTime
+    // generator
+    // icon
+    // image
+    // inReplyTo
+    // location
+    // preview
+    // published
+    // replies 
+    // startTime 
+    // summary 
+    // tag 
+    // updated 
+    // url 
+    // to 
+    // bto 
+    // cc 
+    // bcc
+    // mediaType
+    // duration
+}
+export class ActivityStreamLink {}
+export class ActivityStreamActivity {}
+export class ActivityStreamIntransitiveActivity {}
+export class ActivityStreamCollection {}
+export class ActivityStreamOrderedCollection {}
+export class ActivityStreamCollectionPage {}
+export class ActivityStreamOrderedCollectionPage {}
